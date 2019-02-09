d5ba3032e84749adf7c38e31f8aa9ade71b2617e
Rizaldi Dev <rizaldinurm@gmail.com>
2019-02-09 08:30:44 +0700

    clear barebone

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+export const helloAction = () => {
+  //anything action here
+};
+
+export const helloAsynAwait = () => async dispatch => {
+  //const res = await //TODO
+  //dispatch({})
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import App from "./components/App";
+
+import Reducer from "./reducers";
+
+const store = createStore(Reducer, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
